7a2d66087b4cc342905dfeebad6c41df516328f5
tweimann <t@tweimann.xyz>
2021-12-13 23:15:43 +0100

    imprint path in footer was wrong

--- js/footer.js
+++ js/footer.js
@@ -11,6 +11,6 @@ document.write('\
     <p class="watermark">\
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/tweimann">tweimann</a> 2021 \u2022 Made with \
             <a target="_blank" rel="noopener noreferrer" class="heart" href="https://github.com/tweimann/' + window.location.hostname + '/">\u2764</a> in Germany \u2022 \
-            <a href="imprint.html">Imprint</a>\
+            <a href="https://tweimann.xyz/imprint.html">Imprint</a>\
     </p>\
 </footer>');
