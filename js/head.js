de4bf7391d8285253417a1d615355047cb76f85f
tweimann <t@tweimann.xyz>
2021-12-13 23:23:43 +0100

    final-fix.ico

--- js/head.js
+++ js/head.js
@@ -1,7 +1,7 @@
 const path = window.location.pathname;
 var title = path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf(".html")).replace('index', 'home');
 if (path === '/') {var formattedPath = 'home'};
-var formattedTitle = title.substring(2) + ' | tweimann';
+var formattedTitle = title.substring(1) + ' | tweimann';
 var favicon = title.substring(0, 1) + '.ico';
 
 document.write('\
