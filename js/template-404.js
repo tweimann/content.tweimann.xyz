6eb5e034ff38b6255e942ffe2fb70a322eac3e7c
tweimann <t@tweimann.xyz>
2021-12-14 21:25:50 +0100

    added template-404

---
+++ js/template-404.js
@@ -0,0 +1,17 @@
+document.write('\
+    <head>\
+        <script src="https://content.tweimann.xyz/js/head.js"></script>\
+    </head>\
+    <body>\
+        <h1 align="center">404: Page not found</h1>\
+        <hr>\
+        <div align="center">\
+            <img style="width: 400px; height: auto; filter: brightness(1);" src="https://content.tweimann.xyz/icons/notlikethis.png"/>\
+        </div>\
+        <br>\
+        <div align="center">\
+            <a style="font-size: 20px;" target="_blank" rel="noopener noreferrer" href="https://' + window.location.hostname + '"><h3>HOME</h3></a>\
+        </div>\
+        <script src="https://content.tweimann.xyz/js/footer.js"></script>\
+    </body>\
+');
