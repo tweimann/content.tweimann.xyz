b8353a122ea722152a302b16ff1eab01ef8c83ad
tweimann <t@tweimann.xyz>
2021-12-17 15:03:34 +0100

    404 home link will now open in same tab

--- js/template-404.js
+++ js/template-404.js
@@ -6,11 +6,11 @@ document.write('\
         <h1 align="center">404: Page not found</h1>\
         <hr>\
         <div align="center">\
-            <img style="width: 400px; height: auto; filter: brightness(1);" src="https://content.tweimann.xyz/icons/notlikethis.png"/>\
+            <img style="width: 400px; height: auto;" src="https://content.tweimann.xyz/icons/notlikethis.png"/>\
         </div>\
         <br>\
         <div align="center">\
-            <a style="font-size: 20px;" target="_blank" rel="noopener noreferrer" href="https://' + window.location.hostname + '"><h3>Home</h3></a>\
+            <a style="font-size: 20px;" href="https://' + window.location.hostname + '"><h3>Home</h3></a>\
         </div>\
         <script src="https://content.tweimann.xyz/js/footer.js"></script>\
     </body>\
