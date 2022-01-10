79ef4a2102fa70b7ef4c4e990831bc9a17bfe438
tweimann <t@tweimann.xyz>
2022-01-10 08:52:23 +0100

    Update ip.js

--- js/ip.js
+++ js/ip.js
@@ -1,10 +1,9 @@
-// https://ipdata.co/blog/how-to-get-the-ip-address-in-javascript/
-function text(url) {
-  return fetch(url).then(res => res.text());
-}
-
-text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
-  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
-  let ip = data.match(ipRegex)[0];
-  document.write(ip);
-});
+fetch('https://www.cloudflare.com/cdn-cgi/trace')
+  .then(res => {
+    var resArray = String(res).split('\n');
+    resArray.forEach(e => {
+      if (e.startsWith('ip')) {
+        document.write(e);
+      }
+    });
+  });
