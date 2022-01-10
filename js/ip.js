e5c3f0b63b290d5ef2251920d48f8408b1e34786
tweimann <t@tweimann.xyz>
2022-01-10 09:15:56 +0100

    Update ip.js

--- js/ip.js
+++ js/ip.js
@@ -10,13 +10,3 @@ fetch('https://www.cloudflare.com/cdn-cgi/trace')
 
 fetch('https://www.cloudflare.com/cdn-cgi/trace')
   .then(res => document.write(res));
-
-$.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
-  // Convert key-value pairs to JSON
-  // https://stackoverflow.com/a/39284735/452587
-  data = data.trim().split('\n').reduce(function(obj, pair) {
-    pair = pair.split('=');
-    return obj[pair[0]] = pair[1], obj;
-  }, {});
-  console.log(data);
-});
