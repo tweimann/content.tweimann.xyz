ac05c7e55a227efe3eb05e28fd50f7e607435f57
tweimann <t@tweimann.xyz>
2022-01-14 16:00:25 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,8 +2,6 @@ const path = window.location.pathname;
 var formattedPath = path.replaceAll('/', ' / ').substring(0, path.lastIndexOf(".html") + 2).replace('index', 'home');
 if (path === '/') {var formattedPath = ' / home'};
 
-document.write('\
-\
-<header>\
+document.write('<header>\
     <div class="header-text"><a href="./index.html">' + window.location.hostname.substring(0, window.location.hostname.indexOf('.')) + '</a>' + formattedPath + '</div>\
 </header>');
