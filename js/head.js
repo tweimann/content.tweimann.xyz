76434cd32e56220af54cad4134724d7c8ae08f13
tweimann <t@tweimann.xyz>
2022-02-01 17:01:53 +0100

    test

--- js/head.js
+++ js/head.js
@@ -11,6 +11,4 @@ var favicon = title.substring(0, 1).toLowerCase() + '.ico';
 document.write('<link rel="shortcut icon" type="image/ico" href="https://content.tweimann.xyz/icons/abc/' + favicon + '"/>\
 <title>' + formattedTitle + '</title>\
 <link rel="stylesheet" href="https://content.tweimann.xyz/css/style.css">\
-<link rel="preconnect" href="https://fonts.googleapis.com">\
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\
-<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">');
+<link href="https://content.tweimann.xyz/fonts/Roboto-Regular.ttf" rel="stylesheet">');
